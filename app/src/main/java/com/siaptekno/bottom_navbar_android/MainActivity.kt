50920853856138de46824f7f84f42844c16ca33f
e-haikal <taufanasman@yahoo.com>
2024-07-08 18:33:07 +0700

    first commit

---
+++ app/src/main/java/com/siaptekno/bottom_navbar_android/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.siaptekno.bottom_navbar_android
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(2).isEnabled = false
+
+    }
+}
